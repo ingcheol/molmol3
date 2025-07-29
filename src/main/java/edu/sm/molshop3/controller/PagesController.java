de1a8aace930c500c45208d4848dc1c7ffcc69d6
ingcheol <sic0804@naver.com>
2025-07-29 18:24:45 +0900

    0729

--- src/main/java/edu/sm/molshop3/controller/PagesController.java
+++ src/main/java/edu/sm/molshop3/controller/PagesController.java
@@ -1,24 +1,33 @@
 package edu.sm.molshop3.controller;
 
+import edu.sm.molshop3.dto.Cust;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PagesController {
 
     @GetMapping("/pages/information")
-    public String showInformationPage() {
-        return "/pages/information"; // /views/pages/information.jsp
+    public String showInformationPage(Model model, HttpSession session) {
+        Cust cust = (Cust) session.getAttribute("logincust"); // 여기를 "loginCust" → "logincust"로 수정
+        if (cust == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("cust", cust);
+        return "/pages/information";
     }
+
+
 
     @GetMapping("/pages/orders")
     public String showOrdersPage() {
-        return "/pages/orders"; // /views/pages/orders.jsp
+        return "/pages/orders";
     }
 
     @GetMapping("/pages/review")
     public String showReviewPage() {
-        return "/pages/review"; // /views/pages/review.jsp
+        return "/pages/review";
     }
-
 }
